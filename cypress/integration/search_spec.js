d5a213efa0d9d22a3e0cc49af0c0db16d28fbb1e
Andrew Smith <andrew@andrew.codes>
2018-02-20 16:36:31 -0500

    refactor search tests to generate test by test case

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -1,87 +1,43 @@
-describe('search on homepage', () => {
-  describe('when searching and then deleting search criteria', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000');
-    });
-
-    it('it show no navigation items', () => {
-      cy.get('nav dd').should('have.length', 2);
-      cy
-        .get('input')
-        .type('one')
-        .clear();
-      cy.get('nav dd').should('have.length', 2);
-    });
-  });
-
-  describe('when searching that yields results', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000');
-    });
-
-    it('it filters the navigation to show only pages with keywords matching search criteria', () => {
-      cy.get('nav dd').should('have.length', 2);
-
-      cy.get('input').type('one');
-      cy.get('nav dd').should('have.length', 1);
-      cy.get('nav dd').contains('Test Page 1');
-    });
-  });
-
-  describe('when searching with criteria that yields no results', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000');
-    });
-
-    it('it show no navigation items', () => {
-      cy.get('nav dd').should('have.length', 2);
-
-      cy.get('input').type('no results');
-      cy.get('nav dd').should('have.length', 0);
-    });
-  });
+describe('search', () => {
+  const urls = ['http://localhost:8000', 'http://localhost:8000/test-page-01'];
+  urls.forEach(createTestSearch(describe, it));
 });
 
-describe('search on search result pages', () => {
-  describe('when searching and then deleting search criteria', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000/test-page-01/');
+function createTestSearch(describe, it) {
+  return url => {
+    describe(`searching on page (${url})`, () => {
+      beforeEach(() => {
+        cy.visit(url);
+      });
+      describe('when searching and then deleting search criteria', () => {
+        it('it show no navigation items', () => {
+          cy.get('nav dd').should('have.length', 2);
+          cy
+            .get('input')
+            .type('one')
+            .clear();
+          cy.get('nav dd').should('have.length', 2);
+        });
+      });
+
+      describe('when searching that yields results', () => {
+        it('it filters the navigation to show only pages with keywords matching search criteria', () => {
+          cy.get('nav dd').should('have.length', 2);
+
+          cy.get('input').type('one');
+          cy.get('nav dd').should('have.length', 1);
+          cy.get('nav dd').contains('Test Page 1');
+        });
+      });
+
+      describe('when searching with criteria that yields no results', () => {
+        it('it show no navigation items', () => {
+          cy.get('nav dd').should('have.length', 2);
+
+          cy.get('input').type('no results');
+          cy.get('nav dd').should('have.length', 0);
+        });
+      });
     });
-
-    it('it show no navigation items', () => {
-      cy.get('nav dd').should('have.length', 2);
-      cy
-        .get('input')
-        .type('one')
-        .clear();
-      cy.get('nav dd').should('have.length', 2);
-    });
-  });
-
-  describe('when searching that yields results', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000/test-page-01/');
-    });
-
-    it('it filters the navigation to show only pages with keywords matching search criteria', () => {
-      cy.get('nav dd').should('have.length', 2);
-
-      cy.get('input').type('one');
-      cy.get('nav dd').should('have.length', 1);
-      cy.get('nav dd').contains('Test Page 1');
-    });
-  });
-
-  describe('when searching with criteria that yields no results', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:8000/test-page-01/');
-    });
-
-    it('it show no navigation items', () => {
-      cy.get('nav dd').should('have.length', 2);
-
-      cy.get('input').type('no results');
-      cy.get('nav dd').should('have.length', 0);
-    });
-  });
-});
+  };
+}
